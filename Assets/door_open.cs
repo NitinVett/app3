0f0ae7704b09d2f0bcebb1845a73060b17c2e984
NitinVett <76098798+NitinVett@users.noreply.github.com>
2025-11-20 18:31:26 -0500

    added prompt to open door

--- Assets/door_open.cs
+++ Assets/door_open.cs
@@ -3,21 +3,29 @@ using DG.Tweening;
 public class door_open : MonoBehaviour
 {
     public GameObject door;
-    
+    public GameObject open_prompt;
     void Start()
     {
         
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
-        door.transform.DORotate(
-            new Vector3(90, 90, 0), 
-            1f                     
-        );
-        gameObject.SetActive(false);
+        open_prompt.SetActive(true);
+         if (Input.GetKey(KeyCode.E))
+        {
+            door.transform.DORotate(new Vector3(90, 90, 0), 1f);
+            gameObject.SetActive(false);
+            open_prompt.SetActive(false);
+        }
+        
     }
-    
+
+    void OnTriggerExit(Collider other)
+    {
+        open_prompt.SetActive(false);
+    }
+
 }
 
 
